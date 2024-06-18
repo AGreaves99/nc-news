fbf7d94b86641b5d3614fd620577d55625677974
AGreaves99 <ajohngreaves@outlook.com>
2024-06-18 16:45:28 +0100

    files for rendering all articles

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Articles from "./components/Articles";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import "bootstrap/dist/css/bootstrap.min.css";
+import PageNavigation from "./components/PageNavigation";
+import { useState } from "react";
+import ItemsPerPage from "./components/ItemsPerPage";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [pageNumber, setPageNumber] = useState(1);
+  const [totalPage, setTotalPage] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  pageNumber > totalPage && setPageNumber(totalPage);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header />
+      <Navbar />
+      <ItemsPerPage
+        itemsPerPage={itemsPerPage}
+        setItemsPerPage={setItemsPerPage}
+      />
+      <Articles
+        pageNumber={pageNumber}
+        itemsPerPage={itemsPerPage}
+        setTotalPage={setTotalPage}
+      >
+        <PageNavigation
+          pageNumber={pageNumber}
+          setPageNumber={setPageNumber}
+          totalPage={totalPage}
+        />
+      </Articles>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
