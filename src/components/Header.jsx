65c7b52ebb2efe296fcc4e89af41e426b2e91c82
AGreaves99 <ajohngreaves@outlook.com>
2024-06-18 16:40:51 +0100

    components for viewing all articles + header/navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import "../../styling/Header.css"
+
+function Header() {
+    return <header className="header">
+        <div className="header-titles">
+        <h1>NC News</h1>
+        <h1>Home</h1>
+        <h1>Account</h1>
+        </div>
+    </header>
+}
+
+export default Header
