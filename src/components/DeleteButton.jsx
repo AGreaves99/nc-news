5b6755c7ac5575c370fde7a994aed15de96f83cb
AGreaves99 <ajohngreaves@outlook.com>
2024-06-22 13:11:06 +0100

    disabled delete button when clicked to prevent spamming

--- src/components/DeleteButton.jsx
+++ src/components/DeleteButton.jsx
@@ -1,9 +1,17 @@
 import { Button } from "react-bootstrap";
 import { FaDeleteLeft } from "react-icons/fa6";
 import { deleteComment } from "../../api-calls/api-calls";
+import { useState } from "react";
 
-function DeleteButton({ setComments, comment_id, setCommentToDelete }) {
+function DeleteButton({
+  setComments,
+  comment_id,
+  setCommentToDelete,
+  setArticleData,
+}) {
+  const [loading, setLoading] = useState(false);
   function handleClick(event) {
+    setLoading(true);
     setCommentToDelete(comment_id);
     deleteComment(comment_id).then(() => {
       setTimeout(() => {
@@ -14,10 +22,19 @@ function DeleteButton({ setComments, comment_id, setCommentToDelete }) {
         });
         setCommentToDelete(null);
       }, 300);
+      setLoading(false);
+      setArticleData((currentData) => {
+        return { ...currentData, comment_count: currentData.comment_count - 1 };
+      });
     });
   }
   return (
-    <Button value={comment_id} className="vote-button" onClick={handleClick}>
+    <Button
+      value={comment_id}
+      disabled={loading}
+      className="vote-button"
+      onClick={handleClick}
+    >
       <FaDeleteLeft />
     </Button>
   );
