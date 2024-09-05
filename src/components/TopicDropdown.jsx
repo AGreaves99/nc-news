d657208e5772b9046875f4bdf59e46a8f6df1252
AGreaves99 <ajohngreaves@outlook.com>
2024-09-05 14:53:01 +0100

    dropdown component and styling

---
+++ src/components/TopicDropdown.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import { getTopics } from "../../api-calls/api-calls";
+import { useNavigate } from "react-router-dom";
+import "../../styling/Dropdown.css"; // Import the CSS file
+
+function TopicDropdown() {
+  const [topics, setTopics] = useState([]);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    getTopics().then((topics) => {
+      setTopics(topics);
+    });
+  }, []);
+
+  function handleClick(event) {
+    const topic = event.target.value;
+    navigate(`/${topic}`);
+  }
+
+  return (
+    <select name="topics" id="topic-select" onChange={handleClick}>
+      <option value="">--View by Topic--</option>
+      {topics.map((topic) => (
+        <option key={topic.slug} value={topic.slug}>
+          {topic.slug[0].toUpperCase() + topic.slug.slice(1)}
+        </option>
+      ))}
+    </select>
+  );
+}
+
+export default TopicDropdown;
