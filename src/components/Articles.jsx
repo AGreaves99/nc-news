0101d048f41eff2e4df5c88a114e33951cfb4f9b
AGreaves99 <ajohngreaves@outlook.com>
2024-09-05 14:53:56 +0100

    add topic to params

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,22 +1,24 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { makeArticleList } from "../../api-calls/api-articles-processing";
 import "../../styling/Articles.css";
 
-function Articles({pageNumber, setTotalPage, itemsPerPage, children}) {
+function Articles({ pageNumber, setTotalPage, itemsPerPage, children }) {
   const [articleList, setArticleList] = useState([]);
+  const { topic } = useParams();
 
   useEffect(() => {
     const params = {
-        p: pageNumber,
-        limit: itemsPerPage
-    }
-    makeArticleList({params}).then(([articleCards, total_count]) => {
+      p: pageNumber,
+      limit: itemsPerPage,
+      topic,
+    };
+    makeArticleList({ params }).then(([articleCards, total_count]) => {
       setArticleList(articleCards);
-      setTotalPage(Math.ceil(total_count / itemsPerPage))
+      setTotalPage(Math.ceil(total_count / itemsPerPage));
     });
-  }, [pageNumber, itemsPerPage]);
+  }, [pageNumber, itemsPerPage, topic]);
 
-  
   return (
     <>
       <section className="articles-list">{articleList}</section>
