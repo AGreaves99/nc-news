e3267213e2f9fc487242970bc8507d5ea056000f
AGreaves99 <ajohngreaves@outlook.com>
2024-09-06 12:17:29 +0100

    add sort_by and order to params for article fetching

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -1,23 +1,28 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useSearchParams } from "react-router-dom";
 import { makeArticleList } from "../../api-calls/api-articles-processing";
 import "../../styling/Articles.css";
 
 function Articles({ pageNumber, setTotalPage, itemsPerPage, children }) {
   const [articleList, setArticleList] = useState([]);
+  const [searchParams, setSearchParams] = useSearchParams();
   const { topic } = useParams();
+  const sort_by = searchParams.get("sort_by");
+  const order = searchParams.get("order");
 
   useEffect(() => {
     const params = {
       p: pageNumber,
       limit: itemsPerPage,
       topic,
+      sort_by,
+      order,
     };
     makeArticleList({ params }).then(([articleCards, total_count]) => {
       setArticleList(articleCards);
       setTotalPage(Math.ceil(total_count / itemsPerPage));
     });
-  }, [pageNumber, itemsPerPage, topic]);
+  }, [pageNumber, itemsPerPage, topic, sort_by, order]);
 
   return (
     <>
